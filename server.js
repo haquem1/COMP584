07564cd91da71d859c1b529b50a45f9521f5ada1
haquem1 <maroof.haque@gmail.com>
2017-04-09 18:45:47 -0700

    basic views without angular for member area and login registration pages

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-var app         = require('express')();
+var express     = require('express');
+var path        = require('path');
 var bodyParser  = require('body-parser');
 var morgan      = require('morgan');
+var app         = express();
 
 // get request parameters
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
