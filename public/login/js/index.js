69bf5e0a5e3364a1c46c3c06a493bf89f34862ac
haquem1 <maroof.haque@gmail.com>
2017-04-14 20:30:17 -0700

    removing hash val

--- public/login/js/index.js
+++ public/login/js/index.js
@@ -1,6 +1,6 @@
 $('.message a').click(function(){
+  if(window.location.hash){ //remove hash
+   window.location.href = window.location.href.replace(/#.*/,'');
+ }
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow"); //animate
-   if(window.location.hash){ //remove hash
-    window.location.href = window.location.href.replace(/#.*/,'');
-  }
 });
