a52421cbac0e272ba10bde7dba0bb8a9177ffcc5
haquem1 <maroof.haque@gmail.com>
2017-04-14 20:33:20 -0700

    failed to remove hash

--- public/login/js/index.js
+++ public/login/js/index.js
@@ -1,6 +1,3 @@
 $('.message a').click(function(){
-  if(window.location.hash){ //remove hash
-   window.location.href = window.location.href.replace(/#.*/,'');
- }
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow"); //animate
 });
