d99fbfe3550b0bed71486b7cedcd56fafa8e51bd
haquem1 <maroof.haque@gmail.com>
2017-04-15 02:51:18 -0700

    enabled forced SSL for prod

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express     = require('express');
+var forceSSL    = require('express-force-ssl');
 var path        = require('path');
 var bodyParser  = require('body-parser');
 var morgan      = require('morgan');
@@ -9,10 +10,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// force SSL on production only
+app.use(forceSSL);
+
 // log to console
 app.use(morgan('dev'));
 
-// use forceSSL middleware
-app.use(forceSSL());
+// bundle and connect the api routes under /
+app.use('/', require('./routes'));
 
 module.exports = app;
