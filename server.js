1021f181ce0f957c5aaf137c7fef8520d090353c
haquem1 <maroof.haque@gmail.com>
2017-04-15 03:37:26 -0700

    force ssl for prod env

--- server.js
+++ server.js
@@ -4,25 +4,20 @@ var bodyParser  = require('body-parser');
 var morgan      = require('morgan');
 var app         = express();
 
-// var forceSsl = function (req, res, next) {
-//    if (req.headers['x-forwarded-proto'] !== 'https') {
-//        return res.redirect(['https://', req.get('Host'), req.url].join(''));
-//    }
-//    return next();
-// };
+var forceSsl = function (req, res, next) {
+   if (req.headers['x-forwarded-proto'] !== 'https') {
+       return res.redirect(['https://', req.get('Host'), req.url].join(''));
+   }
+   return next();
+};
 
 var env = process.env.NODE_ENV || 'dev';
+if (env === 'prod') app.use(forceSsl);
 
 // get request parameters
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// app.configure(function () {
-//    if (env === 'prod') {
-//      app.use(forceSsl)
-//    };
-// }
 
 // log to console
 app.use(morgan('dev'));
