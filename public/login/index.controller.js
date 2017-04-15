6bf64a0a1259ed498d980c39b8a856ad6f7f9e88
haquem1 <maroof.haque@gmail.com>
2017-04-15 05:50:58 -0700

    login working, issue: jquery not working well with angular

---
+++ public/login/index.controller.js
@@ -0,0 +1,30 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('Login.IndexController', Controller);
+
+    function Controller($location, AuthenticationService) {
+        var vm = this;
+
+        vm.login = login;
+
+        initController();
+
+        function initController() {
+            // reset login status
+            AuthenticationService.Logout();
+        };
+
+        function login() {
+            AuthenticationService.Login(vm.username, vm.password, function (result) {
+                if (result === true) {
+                    $location.path('/');
+                } else {
+                    vm.error = 'Username or password is incorrect';
+                }
+            });
+        };
+    }
+})();
